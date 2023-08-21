3518fa98e4458cbca7f4f5528d0010976153375d
ike0909 <125224403+ike0909@users.noreply.github.com>
2023-08-20 21:02:43 -0500

    ready to deploy

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,16 @@
+import classes from './Spinner.module.css';
+
+import React, { Component } from 'react'
+
+class Spinner extends Component {
+
+    
+    render() {
+        return (
+            <div className={classes.Loader_container}>
+                <div className={classes.Loader}>Loading...</div> 
+            </div>
+        )
+    }
+}
+export default Spinner;
